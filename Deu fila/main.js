a51687a3743c948cce8b68b9cb595b7324c35de2
Roberto19191 <caio.gomes@maisunifacisa.com.br>
2025-11-27 15:34:02 -0300

    Add files via upload
    Abas de cadastro e login feitas, logo adicionada e tratamento de exceções de entrada feitas

---
+++ Deu fila/main.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if(nameInput.value ===""){
+        alert("Por favor, preencha o seu nome");
+        return;
+    }
+    if(emailInput.value ===""){
+        alert("Por favor, preencha o seu e-mail");
+        return;
+    }
+    if(!validatePassword(passwordInput.value, 8)){
+        alert("A senha precisa ter no mínimo 8 dígitos");
+        return;
+    }
+    form.submit();
+})
+
+function validatePassword(password, minDigit) {
+    if (password.length >= minDigit) {
+        return true
+
+    }
+    return false
+};
